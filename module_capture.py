ca15696971b60c22118952a8345c4394064caf08
oleksii.smirnov <osmirnov@deloitte.com>
2026-02-28 23:55:52 +0200

    Adjust camera reconnect logic

--- module_capture.py
+++ module_capture.py
@@ -21,16 +21,18 @@ class CaptureModule:
 
     def _camera_worker(self, camera_id: str, fps: int):
         buffer = RingBuffer(camera_id, capacity=10, shape=(settings.HEIGHT, settings.WIDTH, settings.CHANNELS))
-        
+        cam = None
         retries = 0
         try:
             while not self._stop_event.is_set():
-                start_time = time.monotonic()
-                cam = FakeCamera(camera_id=camera_id, fps=fps)
                 try:
-                    logger.info(f"[Capture] {camera_id} connected")                    
-                    while not self._stop_event.is_set():
+                    if cam is None:
+                        start_time = time.monotonic()
+                        cam = FakeCamera(camera_id=camera_id, fps=fps)
+                        logger.info(f"[Capture] {camera_id} connected")                    
+                    else:
                         frame = cam.read()
+                        retries = 0
                         
                         # Save every x frame to prevent overload Porcessing service
                         if cam.frame_count % settings.FRAME_SUBSAMPLE == 0:
@@ -47,21 +49,27 @@ class CaptureModule:
                                     "ts": time.time(),
                                     "frame_num": cam.frame_count
                                 })
-                            
+                                
                 except CameraError as e:
-                    uptime = time.monotonic() - start_time
+                    uptime = time.monotonic() - start_time  # type: ignore
                     logger.error(f"[{camera_id}] Disconnected after {uptime:.1f}s: {e}")
-                    retries = 0 if uptime >= settings.STABLE_THRESHOLD else retries + 1
+                    if cam:
+                        cam.release()
+                        cam = None
+                    retries += 1
+                    if retries >= settings.MAX_RECONNECT:
+                        logger.critical(f"[{camera_id}] Camera permanently unavailable.")
+                        break
                     delay = min(settings.MAX_DELAY, settings.BASE_DELAY * 2 ** retries)
-                    logger.info(f"[{camera_id}] Reconnect in {delay:.1f}s")
+                    logger.info(f"[{camera_id}] Will reconnect in {delay:.1f}s")
                     time.sleep(delay)
                 # TODO develop mechanism to restart camera manually
                 except Exception as e:
                     logger.exception(f"Critical error in thread {camera_id}: {e}")
                     break
-                finally:
-                    if cam:
-                        cam.release()
+
+            if cam:
+                cam.release()
 
         finally:
             buffer.close()
